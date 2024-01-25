fe7c6dd6b731a4b5757845acd0af104ddd729740
Lange <langescomputerscience@gmail.com>
2024-01-25 09:04:20 -0500

    Math

---
+++ Math/math.py
@@ -0,0 +1,12 @@
+#Programmer: Mr. Lange
+#Program: Math
+#Date: 1.10.2024
+
+print(9 * 9) #Multiply
+print(9 + 9) #addition
+print(9 - 6) #subtract
+print(9 / 3) #divide
+print(50 ** 3) #exponent
+print(50 % 6) #modulo - takes what is leftover
+print(50 / 6) #division with remainder - Float
+print(50 // 6) #whole number w/ no remainder
